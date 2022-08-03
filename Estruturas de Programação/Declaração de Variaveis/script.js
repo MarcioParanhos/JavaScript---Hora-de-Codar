20b561139db29fdfc6d735e393b65d5416f8a2c6
Marcio Paranhos <marcio.naga@gmail.com>
2022-08-02 22:05:17 -0300

    Variaveis

---
+++ Estruturas de Programação/Declaração de Variaveis/script.js
@@ -0,0 +1,9 @@
+let nome = "Marcio Paranhos"
+let num1 = 25
+let num2 = 58
+
+
+console.log(nome)
+// Concatenando a variavel na string com ``
+console.log(`Meu nome e ${nome}`)
+console.log(num1 * num2)
---
+++ Estruturas de Programação/Mais Formas de declarar Variaveis/script.js
@@ -0,0 +1,6 @@
+var name = "Marcio Vincius de Borba Paranhos"
+console.log(name)
+
+const name2 = "Marcio Vinicius de Borba Paranhos"
+console.log(name2)
+
