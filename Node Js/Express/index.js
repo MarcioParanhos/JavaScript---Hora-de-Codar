8d1884877ceb83b26e7930c9cf33a3fdf0ad4188
Marcio Paranhos <marcio.naga@gmail.com>
2022-08-27 11:30:20 -0300

    Finalização do Modulo de Node JS

--- Node Js/Express/index.js
+++ Node Js/Express/index.js
@@ -6,6 +6,15 @@ app.get("/", function (req, res) {
   res.send("Primeira rota com express");
 });
 
+//Criando uma segunda rota
+app.get("/teste", function (req, res) {
+  res.send("Testand outra rota com express");
+});
+//Criando uma Terceira rota
+app.get("/teste/testando", function (req, res) {
+  res.send("Testando rota dentro de rota com express");
+});
+
 app.listen(3000, function () {
   console.log("A Aplicação esta funcionando na port 3000");
 });
